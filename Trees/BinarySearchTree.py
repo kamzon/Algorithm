9fd36218f4581c40b163432d7735859c89fb19a4
KAMZON <n.kamzon@gmail.com>
2021-01-20 15:33:50 +0100

    Tree: BST delete node frome

--- Trees/BinarySearchTree.py
+++ Trees/BinarySearchTree.py
@@ -71,22 +71,72 @@ def pre_order_print(root):
     pre_order_print(root.r_child)
     
     
+""" Delete node from tree"""
+
+def delete_node(root, data):
+    
+    if (root is None):
+        return root
+        
+    elif(root.data>data):
+        root.l_child = delete_node(root.l_child, data)
+    elif(root.data<data):
+        root.r_child = delete_node(root.r_child, data)
+        
+    else: 
+        
+        # case 1 : leaf node
+        if(root.l_child is None and root.r_child is None):
+            root=None
+            
+        # case 2: node has one child
+        elif(root.r_child is None):
+            root.data = root.l_child.data
+            root.l_child = None
+            
+        elif(root.l_child is None):
+            root.data = root.r_child.data
+            root.r_child = None
+  
+        # case 3: node has both children
+        
+        else:
+            temp = root.r_child
+            
+            while(temp.l_child != None):
+                temp = temp.l_child
+                
+            root.data=temp.data
+            root.r_child = delete_node(root.r_child, temp.data)
+
+
+    return root
+
+
+
     
 """ example"""
 
 tree = Node(10)
 
-in_order_print(tree)
+#in_order_print(tree)
 
 insert(tree, Node(5))
 
 insert(tree, Node(1))
 
-in_order_print(tree)
-pre_order_print(tree)
+#in_order_print(tree)
+#pre_order_print(tree)
 
 insert(tree, Node(15))
 insert(tree, Node(12))
+insert(tree, Node(13))
+insert(tree, Node(14))
+insert(tree, Node(11))
+insert(tree, Node(12.5))
 
 in_order_print(tree)
-pre_order_print(tree)
+#pre_order_print(tree)
+print("delete 1")
+delete_node(tree, 12)
+in_order_print(tree)
